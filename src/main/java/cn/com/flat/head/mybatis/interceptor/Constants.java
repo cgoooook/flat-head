7c8b0b431033cf6ec0f8f5353e4f55fce185b0f1
poney <panzhuowensky@gmail.com>
2019-10-05 20:46:57 +0800

    包名调整

---
+++ src/main/java/cn/com/flat/head/mybatis/interceptor/Constants.java
@@ -0,0 +1,14 @@
+/**
+ * 类的描述信息
+ *
+ * @author panzhuowen
+ * @version 1.0.1
+ */
+package cn.com.flat.head.mybatis.interceptor;
+
+interface Constants {
+
+    String SIGNATURE_PREPARE = "prepare";
+
+    String HANDLE_RESULT_SETS = "handleResultSets";
+}
