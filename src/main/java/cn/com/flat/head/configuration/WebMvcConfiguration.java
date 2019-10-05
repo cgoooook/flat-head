7342f000520434bfb2dc827b2cd2f7ca76a064f8
poney <panzhuowensky@gmail.com>
2019-10-05 15:14:59 +0800

    国际化配置,登陆页面国际化

--- src/main/java/cn/com/flat/head/configuration/WebMvcConfiguration.java
+++ src/main/java/cn/com/flat/head/configuration/WebMvcConfiguration.java
@@ -1,8 +1,15 @@
 package cn.com.flat.head.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 /**
  * Created by poney on 2019-09-30.
@@ -15,5 +22,26 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        // 默认语言
+        slr.setDefaultLocale(Locale.SIMPLIFIED_CHINESE);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        // 参数名
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
--- src/main/resources/static/pages/scripts/login.js
+++ src/main/resources/static/pages/scripts/login.js
@@ -1,88 +1,93 @@
 var Login = function () {
 
-	var handleLogin = function() {
-		$('.login-form').validate({
-	            errorElement: 'span', //default input error message container
-	            errorClass: 'help-block', // default input error message class
-	            focusInvalid: false, // do not focus the last invalid input
-	            rules: {
-	                username: {
-	                    required: true
-	                },
-	                password: {
-	                    required: true
-	                },
-	                remember: {
-	                    required: false
-	                }
-	            },
+    var handleLogin = function () {
+        $('.login-form').validate({
+            errorElement: 'span', //default input error message container
+            errorClass: 'help-block', // default input error message class
+            focusInvalid: false, // do not focus the last invalid input
+            rules: {
+                username: {
+                    required: true
+                },
+                password: {
+                    required: true
+                },
+                remember: {
+                    required: false
+                }
+            },
 
-	            messages: {
-	                username: {
-	                    required: "Username is required."
-	                },
-	                password: {
-	                    required: "Password is required."
-	                }
-	            },
+            messages: {
+                username: {
+                    required: "Username is required."
+                },
+                password: {
+                    required: "Password is required."
+                }
+            },
 
-	            invalidHandler: function (event, validator) { //display error alert on form submit   
-	                $('.alert-danger', $('.login-form')).show();
-	            },
+            invalidHandler: function (event, validator) { //display error alert on form submit
+                $('.alert-danger', $('.login-form')).show();
+            },
 
-	            highlight: function (element) { // hightlight error inputs
-	                $(element)
-	                    .closest('.form-group').addClass('has-error'); // set error class to the control group
-	            },
+            highlight: function (element) { // hightlight error inputs
+                $(element)
+                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+            },
 
-	            success: function (label) {
-	                label.closest('.form-group').removeClass('has-error');
-	                label.remove();
-	            },
+            success: function (label) {
+                label.closest('.form-group').removeClass('has-error');
+                label.remove();
+            },
 
-	            errorPlacement: function (error, element) {
-	                error.insertAfter(element.closest('.input-icon'));
-	            },
+            errorPlacement: function (error, element) {
+                error.insertAfter(element.closest('.input-icon'));
+            },
 
-	            submitHandler: function (form) {
-	                form.submit();
-	            }
-	        });
+            submitHandler: function (form) {
+                form.submit();
+            }
+        });
 
-	        $('.login-form input').keypress(function (e) {
-	            if (e.which === 13) {
-	                if ($('.login-form').validate().form()) {
-	                    $('.login-form').submit();
-	                }
-	                return false;
-	            }
-	        });
-	};
-    
+        $('.login-form input').keypress(function (e) {
+            if (e.which === 13) {
+                if ($('.login-form').validate().form()) {
+                    $('.login-form').submit();
+                }
+                return false;
+            }
+        });
+
+        $("#language").on('change', function () {
+			var lang = $(this).val();
+			location.href = "/login?lang=" + lang;
+		})
+    };
+
     return {
         //main function to initiate the module
         init: function () {
-        	
+
             handleLogin();
 
             // init background slide images
-		    $.backstretch([
-		        "/pages/media/1.jpg",
-		        "/pages/media/2.jpg",
-		        "/pages/media/3.jpg",
-		        "/pages/media/4.jpg"
-		        ], {
-		          fade: 1000,
-		          duration: 8000
-		    	}
-        	);
+            $.backstretch([
+                    "/pages/media/1.jpg",
+                    "/pages/media/2.jpg",
+                    "/pages/media/3.jpg",
+                    "/pages/media/4.jpg"
+                ], {
+                    fade: 1000,
+                    duration: 8000
+                }
+            );
 
-		    console.log(flat.i18n("login.title"))
+            console.log(flat.i18n("login.title"))
         }
     };
 
 }();
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     Login.init();
 });
