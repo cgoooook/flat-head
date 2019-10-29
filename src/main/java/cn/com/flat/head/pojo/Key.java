e6f827eb5ebd7d1323d0702e92aa08a5aeb42862
panzhuowen <panzhuowensky@gmail.com>
2019-10-29 19:16:30 +0800

    keys sql add and mapper add

---
+++ src/main/java/cn/com/flat/head/pojo/Key.java
@@ -0,0 +1,33 @@
+package cn.com.flat.head.pojo;
+
+import lombok.Data;
+
+/**
+ * Created by panzhuowen on 2019/10/29.
+ */
+@Data
+public class Key {
+
+    private String keyId;
+
+    private String keyName;
+
+    private String keyAlg;
+
+    private int length;
+
+    private  int version;
+
+    private String checkValue;
+
+    private String keyValue;
+
+    private String createBy;
+
+    private String templateId;
+
+    private String orgId;
+
+    private int status;
+
+}
