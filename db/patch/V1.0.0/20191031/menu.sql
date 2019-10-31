746b9db8e21dbedc64000c03efe64a5d1c48c5da
panjunhong <Aa123456>
2019-10-31 21:14:54 +0800

    新增设备菜单

---
+++ db/patch/V1.0.0/20191031/menu.sql
@@ -0,0 +1,10 @@
+INSERT INTO `kms`.`sys_acl_menu` (`menu_id`, `menu_name`, `menu_icon`, `parent_id`, `menu_url`, `level`, `weight`,
+                                  `disabled`, `local`, `leaf`, `perm_token`)
+VALUES ('' 03 '', '' 设备管理 '', NULL, '' - 1 '', NULL, '' 0 '', '' 1 '', '' 0 '', NULL, '' 0 '', '' 123 '');
+INSERT INTO `kms`.`sys_acl_menu` (`menu_id`, `menu_name`, `menu_icon`, `parent_id`, `menu_url`, `level`, `weight`,
+                                  `disabled`, `local`, `leaf`, `perm_token`)
+VALUES ('' 0301 '', '' 设备管理 '', NULL, '' 03 '', NULL, '' 1 '', '' 1 '', '' 0 '', NULL, '' 0 '', '' 123 '');
+INSERT INTO `kms`.`sys_acl_menu` (`menu_id`, `menu_name`, `menu_icon`, `parent_id`, `menu_url`, `level`, `weight`,
+                                  `disabled`, `local`, `leaf`, `perm_token`)
+VALUES ('' 030101 '', '' 设备管理 '', NULL, '' 0301 '', '' / sys / device '', '' 2 '', '' 1 '', '' 0 '', NULL, '' 1 '',
+        '' 123 '');
