28508ed647936411fb3694afb1f105a2fb4ec96d
panzhuowen <panzhuowensky@gmail.com>
2019-11-05 20:25:13 +0800

    产生密钥，分散密钥

--- src/main/java/cn/com/flat/head/crypto/CryptoInstance.java
+++ src/main/java/cn/com/flat/head/crypto/CryptoInstance.java
@@ -7,6 +7,10 @@ import cn.com.flat.head.sdf.cipher.SMechanism;
 import cn.com.flat.head.sdf.util.Arrays;
 import cn.com.flat.head.sdf.util.encoders.Base64;
 import cn.com.flat.head.sdf.util.encoders.Hex;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.MessageDigest;
+import java.security.Security;
 
 /**
  * Created by panzhuowen on 2019/10/28.
@@ -38,10 +42,7 @@ public class CryptoInstance {
             FSecretKey fSecretKey = new FSecretKey();
             SKey sKey = session.generateKey(new SMechanism(covertSymAlgGen(algorithm)), length);
             byte[] key = sKey.getKey();
-            byte[] encrypt = session.encrypt(new SMechanism(SMechanism.SKM_SM4_ECB),
-                    sKey, getCheckValueInput(length));
-            byte[] checkValue = new byte[8];
-            System.arraycopy(encrypt, 0, checkValue, 0, 8);
+            byte[] checkValue = getCheckValue(algorithm, length, sKey);
             fSecretKey.setAlgorithm(algorithm);
             fSecretKey.setKey(key);
             fSecretKey.setCheckValue(checkValue);
@@ -52,8 +53,34 @@ public class CryptoInstance {
         return null;
     }
 
-    public FSecretKey driveKey(FSecretKey secretKey, String[] drives) throws CryptoException {
+    private byte[] getCheckValue(String algorithm, int length, SKey sKey) throws Exception {
+        byte[] encrypt = session.encrypt(new SMechanism(covertSymAlgEnc(algorithm)),
+                sKey, getCheckValueInput(length));
+        byte[] checkValue = new byte[8];
+        System.arraycopy(encrypt, 0, checkValue, 0, 8);
+        return checkValue;
+    }
 
+    public FSecretKey deriveKey(FSecretKey secretKey, String derives) throws CryptoException {
+        int algEnc = covertSymAlgEnc(secretKey.getAlgorithm());
+        byte[] key = null;
+        SKey sKey = new SKey(SKey.SKK_SM4_KEY);
+        sKey.setKey(secretKey.getKey());
+        byte[] deriveCompent = getDeriveCompent(derives);
+        try {
+            byte[] encrypt = session.encrypt(new SMechanism(algEnc), sKey, deriveCompent);
+            SKey dk = new SKey(SKey.SKK_SM4_KEY);
+            dk.setKey(encrypt);
+            FSecretKey fSecretKey = new FSecretKey();
+            fSecretKey.setLength(secretKey.getLength());
+            fSecretKey.setAlgorithm(secretKey.getAlgorithm());
+            fSecretKey.setKey(encrypt);
+            byte[] checkValue = getCheckValue(fSecretKey.getAlgorithm(), fSecretKey.getLength(), dk);
+            fSecretKey.setCheckValue(checkValue);
+            return fSecretKey;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
@@ -62,10 +89,17 @@ public class CryptoInstance {
         return null;
     }
 
-    public static void main(String[] args) {
-        FSecretKey fSecretKey = CryptoInstance.getCryptoInstance().generateKey("SM4", 128);
-        System.out.println(new String(Hex.encode(fSecretKey.getKey())));
-        System.out.println(new String(Hex.encode(fSecretKey.getCheckValue())));
+
+
+    public static void main(String[] args) throws Exception {
+//        FSecretKey fSecretKey = CryptoInstance.getCryptoInstance().generateKey("SM4", 128);
+//        System.out.println(new String(Hex.encode(fSecretKey.getKey())));
+//        System.out.println(new String(Hex.encode(fSecretKey.getCheckValue())));
+        Security.addProvider(new BouncyCastleProvider());
+        MessageDigest digest = MessageDigest.getInstance("MD5", "BC");
+        digest.update("123".getBytes());
+        byte[] digest1 = digest.digest();
+        System.out.println(digest1.length);
     }
 
     private int covertSymAlgGen(String alg) {
@@ -77,7 +111,30 @@ public class CryptoInstance {
         return -1;
     }
 
-    public byte[] getCheckValueInput(int length) {
+    private byte[] getDeriveCompent(String derive) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5", "BC");
+            digest.update(derive.getBytes());
+            return digest.digest();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private int covertSymAlgEnc(String alg) {
+        switch (alg) {
+            case "SM4":
+                return SMechanism.SKM_SM4_ECB;
+            case "AES":
+                return -2;
+        }
+
+        return -1;
+    }
+
+    private byte[] getCheckValueInput(int length) {
         int len = length / 8;
         byte[] input = new byte[len];
         Arrays.fill(input, (byte)0x0);
