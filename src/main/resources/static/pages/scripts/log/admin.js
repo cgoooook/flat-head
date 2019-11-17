4041460523c395802c9845b12560b8282c9f96bb
panjunhong <Aa123456>
2019-11-17 15:41:50 +0800

    管理日志

--- src/main/resources/static/pages/scripts/log/admin.js
+++ src/main/resources/static/pages/scripts/log/admin.js
@@ -28,6 +28,20 @@ var adminLog = function () {
                 "paging": false
             }
         });
+
+        function initDatePicker() {
+            $("input.datepicker").datetimepicker({
+                isRTL: App.isRTL(),
+                language: DATETIME_PICKER,
+                format: "yyyy-mm-dd HH:mm:ss",
+                autoclose: true,
+                todayBtn: true,
+                pickerPosition: (App.isRTL() ? "bottom-right" : "bottom-left"),
+                minuteStep: 10
+            });
+        }
+
+
     };
 
 
