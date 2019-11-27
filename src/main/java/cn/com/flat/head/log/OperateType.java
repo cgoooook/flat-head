916cdb283fcc8e4a8fd3fb636623d142567fb96a
panzhuowen <panzhuowensky@gmail.com>
2019-11-27 18:53:24 +0800

    报错处理

--- src/main/java/cn/com/flat/head/log/OperateType.java
+++ src/main/java/cn/com/flat/head/log/OperateType.java
@@ -29,14 +29,17 @@ public interface OperateType {
      * 添加机构
      */
     String addOrg = "addOrg";
+
     /**
      * 更新机构
      */
     String updateOrg = "updateOrg";
+
     /**
      * 删除机构
      */
     String deleteOrg = "deleteOrg";
+
     /**
      * 导出机构数据
      */
@@ -57,5 +60,105 @@ public interface OperateType {
      */
     String deleteTemplate = "deleteTemplate";
 
+    /**
+     * 添加密钥
+     */
+    String addKey = "addKey";
+
+    /**
+     * 更新密钥
+     */
+    String updateKey = "updateKey";
+
+    /**
+     * 归档密钥
+     */
+    String archiveKey = "archiveKey";
+
+    /**
+     * 启用密钥
+     */
+    String enableKey = "enableKey";
+
+    /**
+     * 禁用密钥
+     */
+    String disableKey = "disableKey";
+
+    /**
+     * 删除密钥
+     */
+    String deleteKey = "deleteKey";
+
+    /**
+     * 导入密钥
+     */
+    String importKey = "importKey";
+
+    /**
+     * 添加密钥集
+     */
+    String addCollection = "addCollection";
+
+    /**
+     * 更新密钥集
+     */
+    String updateCollection = "updateCollection";
+
+    /**
+     * 删除密钥集
+     */
+    String deleteCollection = "deleteCollection";
+
+    /**
+     * 添加设备
+     */
+    String addDevice = "addDevice";
+
+    /**
+     * 更新设备
+     */
+    String updateDevice = "updateDevice";
+
+    /**
+     * 删除设备
+     */
+    String deleteDevice = "deleteDevice";
+
+    /**
+     * 数据库配置
+     */
+    String jdbcConfig = "jdbcConfig";
+
+    /**
+     * 日志配置
+     */
+    String logConfig = "logConfig";
+
+    /**
+     * ui配置
+     */
+    String uiConfig = "uiConfig";
+
+    /**
+     * 邮件配置
+     */
+    String mailConfig = "mailConfig";
+
+    /**
+     * 添加任务
+     */
+    String addTeak = "addTask";
+
+    /**
+     * 更新任务
+     */
+    String updateTask = "updateTask";
+
+    /**
+     * 删除任务
+     */
+    String deleteTask = "deleteTask";
 
 }
+
