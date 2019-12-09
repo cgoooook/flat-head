a8374ff8e13a10d7635c04e0324a27741852f8b5
panzhuowen <panzhuowensky@gmail.com>
2019-12-09 23:50:06 +0800

    日志样式更改

--- src/main/resources/static/pages/scripts/log/admin.js
+++ src/main/resources/static/pages/scripts/log/admin.js
@@ -9,10 +9,13 @@ var adminLog = function () {
         grid.init({
             src: $table,
             onQuery:function (data) {
+                var operationTime = $("#defaultrange input").val();
+                var ationTime = operationTime.split(" - ")[0];
+                var endTime = operationTime.split(" - ")[1];
                 data.operateType=$("#operationType option:selected").val();
                 data.operatorResult=Number($("#operateResultQuery option:selected").val());
-                data.operateTimeBegin=$("#beginTime").val();
-                data.operateTimeEnd=$("#endTime").val();
+                data.operateTimeBegin=ationTime;
+                data.operateTimeEnd=endTime;
 
             },
             dataTable: {
@@ -50,15 +53,36 @@ var adminLog = function () {
     };
 
         function initDatePicker() {
-            $("input.datepicker").datetimepicker({
-                isRTL: App.isRTL(),
-                language: DATETIME_PICKER,
-                format: "yyyy-mm-dd HH:mm:ss",
-                autoclose: true,
-                todayBtn: true,
-                pickerPosition: (App.isRTL() ? "bottom-right" : "bottom-left"),
-                minuteStep: 10
-            });
+            $('#defaultrange').daterangepicker({
+                    opens: (App.isRTL() ? 'left' : 'right'),
+                    separator: ' 至 ',
+                    startDate: moment().subtract('days', 29),
+                    endDate: moment(),
+                    ranges: {
+                        '今天': [moment(), moment()],
+                        '昨天': [moment().subtract('days', 1), moment().subtract('days', 1)],
+                        '最近7天': [moment().subtract('days', 6), moment()],
+                        '最近30天': [moment().subtract('days', 29), moment()],
+                        '这个月': [moment().startOf('month'), moment().endOf('month')],
+                        '上个月': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+                    },
+                    locale: {
+                        format: "YYYY-MM-DD HH:mm:ss",
+                        applyLabel: '确认',
+                        cancelLabel: '清除',
+                        fromLabel: '从',
+                        toLabel: '至',
+                        customRangeLabel: '自定义范围',
+                        daysOfWeek: ['日', '一', '二', '三', '四', '五', '六'],
+                        monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+                        firstDay: 1
+                    },
+                },
+                function (start, end) {
+                    $('#defaultrange input').val(start.format('YYYY-MM-DD HH:mm:ss') + ' - ' + end.format('YYYY-MM-DD HH:mm:ss'));
+                }
+            );
+            $('#defaultrange input').val(moment().subtract('days', 29).format('YYYY-MM-DD HH:mm:ss') + ' - ' + moment().format('YYYY-MM-DD HH:mm:ss'));
         }
 
 
